9008ffdae21fc7590130bf01a25c3bfaea237f16
Samuel Rey <samuelrparaja@gmail.com>
2026-02-16 20:26:10 +0100

    change for ex3

--- src/test/Ex1.java
+++ src/test/Ex1.java
@@ -1,7 +1,7 @@
 package test;
 
 public class Ex1 {
-	// This is a new change in branch Ex1, which then got changed in master
+	//This is a new change in branch Ex1, which then got changed in master and ex3
 	
 	// After all that, master added a line down here
 }
